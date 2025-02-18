8bd793107c5fc408018665e7e8e2bfa7ab837369
Akin Akinbobola <akin.akinbobola@nomba.com>
2025-02-18 15:08:25 +0100

    add transaction interface and implement delete functionality in TransactionPage

--- frontend/src/pages/TransactionPage.tsx
+++ frontend/src/pages/TransactionPage.tsx
@@ -1,14 +1,28 @@
-import {useQuery} from "@tanstack/react-query";
+import {useMutation, useQuery, useQueryClient} from "@tanstack/react-query";
 import {axiosInstance} from "@/axiosInstance.ts";
+import {Transaction} from "@/types/Transaction.ts";
+import {Button} from "@/components/ui/button.tsx";
 
 const TransactionPage = () => {
+    const queryClient = useQueryClient()
+
     const transactions = useQuery({
         queryKey: ["transactions"],
         queryFn: async () => {
-            const response = await axiosInstance('/transactions')
+            const response = await axiosInstance<Transaction[]>('/transactions')
             return response.data
         }
     })
+
+    const deleteTransactionMutation = useMutation({
+        mutationFn: async (id: number) => {
+            await axiosInstance.delete(`/transactions/${id}`)
+        },
+        onSuccess: () => {
+            queryClient.invalidateQueries({queryKey: ["transactions"]})
+        }
+    })
+
 
     return (
         <div>
@@ -17,13 +31,16 @@ const TransactionPage = () => {
             {transactions.isError && <p>Error</p>}
             {transactions.isSuccess && (
                 <ul>
-                    {transactions.data.map((transaction: any) => (
+                    {transactions.data.map((transaction: Transaction) => (
                         <li key={transaction.id}>
                             {transaction.description} - {transaction.amount}
+                            <Button onClick={() => deleteTransactionMutation.mutate(transaction.id)}>Delete</Button>
                         </li>
                     ))}
                 </ul>
             )}
+
+            <Button>Add transaction</Button>
         </div>
     );
 };
---
+++ frontend/src/types/Transaction.ts
@@ -0,0 +1,8 @@
+export interface Transaction {
+    id: number
+    amount: number
+    description: string
+    category: string
+    date: string
+    type: string
+}
