4840fc9af785c475810fa1e337fdf35106c88d9f
Akin Akinbobola <akin.akinbobola@nomba.com>
2025-02-19 16:25:26 +0100

    feat: update Navbar for mobile support and adjust Layout padding

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -2,7 +2,7 @@ import {Outlet} from "react-router";
 
 const Layout = () => {
     return (
-        <div className="container mx-auto px-4 sm:px-6 lg:px-8 min-h-screen flex flex-col pt-[56px]">
+        <div className="container mx-auto px-4 sm:px-6 lg:px-8 min-h-screen flex flex-col pt-6  md:pt-[56px]">
             <Outlet/>
         </div>
     );
--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,25 +1,44 @@
 import {Link, NavLink} from "react-router";
 import {Button} from "@/components/ui/button.tsx";
+import {Sheet, SheetContent, SheetHeader, SheetTrigger,} from "@/components/ui/sheet"
+import {Menu} from "lucide-react";
 
 const Navbar = () => {
     const hover = "hover:text-blue-700/60";
 
     return (
         <header className={"flex items-center justify-between"}>
-            <img src="/logo.png" alt="Logo" className={"w-[162px] h-[42px]"}/>
+            <img src="/logo.png" alt="Logo" className={"w-[162px] h-[42px] hidden md:block"}/>
 
-            <nav className={"flex items-center justify-between gap-x-10 text-blue-700 font-medium "}>
-                <NavLink to={"/"} className={hover}>
-                    Product
-                </NavLink>
+            <Sheet>
+                <SheetTrigger>
+                    <Button size={"icon"} variant={"ghost"} className={"md:hidden"}>
+                        <Menu/>
+                    </Button>
+                </SheetTrigger>
+                <SheetContent side={"left"}>
+                    <SheetHeader>
+                        <div className={"py-6"}>
+                            <img src="/logo.png" alt="Logo" className={"w-fit"}/>
+                        </div>
+                    </SheetHeader>
+                </SheetContent>
+            </Sheet>
 
-                <NavLink to={"/"} className={hover}>
-                    Services
-                </NavLink>
+            <nav className={"flex items-center justify-between gap-x-10"}>
+                <div className={"items-center justify-between gap-x-10 hidden md:flex text-blue-700 font-medium "}>
+                    <NavLink to={"/"} className={hover}>
+                        Product
+                    </NavLink>
 
-                <NavLink to={"/"} className={hover}>
-                    About
-                </NavLink>
+                    <NavLink to={"/"} className={hover}>
+                        Services
+                    </NavLink>
+
+                    <NavLink to={"/"} className={hover}>
+                        About
+                    </NavLink>
+                </div>
 
                 <Button className={"rounded-full px-10 py-2"} asChild>
                     <Link to={"/transactions"}>
