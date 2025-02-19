fafef8675284836347d5d13e3c51c30ef860f904
Akin Akinbobola <akin.akinbobola@nomba.com>
2025-02-19 22:48:09 +0100

    feat: simplify Footer component by removing NavLink elements and unused imports

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,30 +1,9 @@
-import {NavLink} from "react-router";
 import {Separator} from "@/components/ui/separator.tsx";
 import {Mail, MessageCircle} from "lucide-react";
 
 const Footer = () => {
-    const hover = "hover:text-blue-700/60";
-
     return (
         <div className={"flex flex-col gap-7 py-8"}>
-            <div className={"flex justify-between items-center text-blue-700"}>
-                <img src="/logo.png" alt="Logo" className={"w-[150px] h-[42px]"}/>
-
-                <div className={"flex gap-8"}>
-                    <NavLink to={"/"} className={hover}>
-                        Product
-                    </NavLink>
-
-                    <NavLink to={"/"} className={hover}>
-                        Services
-                    </NavLink>
-
-                    <NavLink to={"/"} className={hover}>
-                        About
-                    </NavLink>
-                </div>
-            </div>
-
             <Separator/>
 
             <div className={"flex justify-between"}>
