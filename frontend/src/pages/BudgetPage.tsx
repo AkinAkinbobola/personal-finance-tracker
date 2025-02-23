d8b9f03b4e68be9990c363b516cceea11cc52107
Akin Akinbobola <akin.akinbobola@nomba.com>
2025-02-23 17:07:27 +0100

    feat: enhance BudgetPage to fetch and display categories using react-query

--- frontend/src/pages/BudgetPage.tsx
+++ frontend/src/pages/BudgetPage.tsx
@@ -1,8 +1,28 @@
+import {useQuery} from "@tanstack/react-query";
+import {axiosInstance} from "@/axios/axiosInstance.ts";
+import {Category} from "@/types/Category.ts";
+
 const BudgetPage = () => {
+    const categories = useQuery({
+        queryKey: ["categories"],
+        queryFn: async () => {
+            const response = await axiosInstance<Category[]>('/categories')
+            return response.data
+        }
+    })
+
     return (
-        <div>
-            BudgetPage
-        </div>
+        <main className={"px-2 py-2 md:py-4 sm:px-6 lg:px-8"}>
+            {categories.isLoading && <p>Loading...</p>}
+            {categories.isError && <p>Error</p>}
+            {categories.isSuccess && (
+                <ul>
+                    {categories.data.map((category) => (
+                        <li key={category.id}>{category.name}</li>
+                    ))}
+                </ul>
+            )}
+        </main>
     );
 };
 
