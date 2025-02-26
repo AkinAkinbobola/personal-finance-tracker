a69ca801819ef5c27b26d5b5fe03f4d30c669ada
Akin Akinbobola <akin.akinbobola@nomba.com>
2025-02-26 12:04:25 +0100

    feat: update Dashboard and Reports pages to use <main> for improved semantic structure

--- frontend/src/pages/ReportsPage.tsx
+++ frontend/src/pages/ReportsPage.tsx
@@ -1,8 +1,8 @@
 const ReportsPage = () => {
     return (
-        <div>
+        <main className={"px-2 py-2 md:py-4 sm:px-6 lg:px-8 h-full"}>
             ReportsPage
-        </div>
+        </main>
     );
 };
 
