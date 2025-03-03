181f259b430045f651a890ed9efcdb6f1d631c80
Akin Akinbobola <akin.akinbobola@nomba.com>
2025-03-03 15:21:39 +0100

    feat: add ReportTable component to display financial reports and update ReportsPage layout

---
+++ frontend/src/components/shared/ReportTable.tsx
@@ -0,0 +1,23 @@
+import {Card, CardContent, CardDescription, CardHeader, CardTitle} from "@/components/ui/card.tsx";
+
+const ReportTable = () => {
+    return (
+        <Card>
+            <CardHeader>
+                <CardTitle>
+                    Financial Reports
+                </CardTitle>
+                <CardDescription>
+                    Analyze your income and expenses over time, filter by categories, and export your reports in common
+                    formats
+                </CardDescription>
+            </CardHeader>
+
+            <CardContent>
+
+            </CardContent>
+        </Card>
+    );
+};
+
+export default ReportTable;
--- frontend/src/pages/ReportsPage.tsx
+++ frontend/src/pages/ReportsPage.tsx
@@ -1,8 +1,11 @@
 import IncomeExpenseReport from "@/components/shared/IncomeExpenseReport.tsx";
+import ReportTable from "@/components/shared/ReportTable.tsx";
 
 const ReportsPage = () => {
     return (
-        <main className={"px-2 py-2 md:py-4 sm:px-6 lg:px-8 h-full"}>
+        <main className={"px-2 py-2 md:py-4 sm:px-6 lg:px-8 h-full space-y-4"}>
+            <ReportTable/>
+
             <IncomeExpenseReport/>
         </main>
     );
